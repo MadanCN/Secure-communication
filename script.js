9db988581b7ca825633e30d4ce4acd84c9ca4412
Madan C N <128129601+MadanCN@users.noreply.github.com>
2024-02-05 23:06:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function chooseFile(fileType) {
+    document.getElementById('file-options').style.display = 'none';
+    document.getElementById('encryption-form').style.display = 'block';  
+  }
+  
