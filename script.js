f3e6a7076922520986142e50cc11241871645fbe
Madan C N <128129601+MadanCN@users.noreply.github.com>
2024-02-13 10:20:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function chooseFile(fileType) {
-    document.getElementById('file-options').style.display = 'none';
-    document.getElementById('encryption-form').style.display = 'block';  
-  }
-  
